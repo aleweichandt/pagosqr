bb13fe5324a81008a990c89e15d8f0c1ccbe0f8a
Fernando Basello <>
2018-07-01 20:58:28 -0300

    boton de share en facebook

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import {
+  FacebookShareButton,
+  FacebookShareCount,
+  FacebookIcon
+} from 'react-share';
 
 const styles = theme => ({
   root: {
@@ -18,6 +23,19 @@ const styles = theme => ({
     position: 'fixed',
     right: 10,
     bottom: 10,
+  },
+  shareButton:{
+    cursor: 'pointer',
+  },
+  shareCount: {
+    marginTop: 3,
+    fontSize: 12,
+  },
+  buttonContainer: {
+    verticalAlign: "top",
+    display: "inline-block",
+    marginRight: 30,
+    textAlign: 'center'
   }
 });
 
@@ -26,6 +44,21 @@ const Footer = (props) => {
   return (
     <div>
       <Paper className={classes.root} elevation={1}>
+        <div className={classes.buttonContainer}>
+          <FacebookShareButton
+              url={"https://pagosqr-web.firebaseapp.com/"}
+              quote={"Genera tu codigo de pagoQR"}
+              className={classes.shareButton}>
+              <FacebookIcon
+                size={32}
+                round />
+            </FacebookShareButton>
+            <FacebookShareCount
+              url={"https://pagosqr-web.firebaseapp.com/"}
+              className={classes.shareCount}>
+              {count => count}
+            </FacebookShareCount>
+        </div>
         <Typography component="p" className={classes.made}>
           made by Mates Inc.
         </Typography>
