37d43ddb5981811ee4e990ab486ca156f99b3ad0
Alejandro Weichandt <aleweichandt@gmail.com>
2018-07-01 20:06:47 -0300

    added validations

--- src/component/QRForm.js
+++ src/component/QRForm.js
@@ -30,42 +30,77 @@ const styles = theme => ({
   }
 });
 
+const aliasPattern = /^[a-zA-Z0-9.-]{6,20}$/;
+
 class QRForm extends React.PureComponent {
 
   state = {
     name:'',
+    nameError: false,
     cuit:'',
-    alias:''
+    cuitError: false,
+    alias:'',
+    aliasError: false,
   };
 
   handleChange = name => event => {
     this.setState({
       [name]: event.target.value,
+      [`${name}Error`]: false,
     });
   };
 
+  validCuit() {
+    const { onError } = this.props;
+    const { cuit } = this.state;
+    const digits = cuit.trim().split('').map(c => parseInt(c));
+    if(digits.length == 11) {
+      const vDigit = digits.pop();
+      const verif = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2];
+      const result = verif.reduce((acc, v, i) => acc + (v * digits[i]), 0);
+      const verifDigit = 11 - (result % 11);
+      if (verifDigit === vDigit) {
+        return true;
+      }
+    }
+    this.setState({ cuitError: true });
+    onError(Error('cuit invalido'));
+    return false;
+  }
+
+  validateAlias() {
+    const { onError } = this.props;
+    const { alias } = this.state;
+    const valid = aliasPattern.test(alias);
+    if(!valid) {
+      this.setState({ aliasError: true });
+      onError(Error('alias invalido'));
+    }
+    return valid;
+  }
+
   onSummit(event) {
     event.preventDefault();
+    return this.validCuit() && this.validateAlias() && this.generate();
+  }
+
+  generate() {
+    const { onComplete, onError } = this.props;
     const {name, cuit, alias} = this.state;
     const options = {
       type: 'commit',
-
     } 
     const queryPath = 'api/generate';
     // const queryPath = 'http://localhost:5000/api/generate';
-    apiCall(queryPath,
-            {name, cuit, alias, city:'CABA'}, 
-            options)
-      .then((res) => {
-        this.props.onComplete(res);
-      })
-      .catch((err) => {
-        this.props.onError(err);
-      });
+    const body = { name, cuit, alias, city: 'CABA' };
+    apiCall(queryPath, body, options)
+      .then((res) => onComplete(res))
+      .catch((err) => onError(err));
   }
 
   render() {
     const { classes } = this.props;
+    const { cuitError, aliasError } = this.state;
     return (
       <React.Fragment>
         <Paper
@@ -74,7 +109,7 @@ class QRForm extends React.PureComponent {
           component="form"
           onSubmit={(event) => this.onSummit(event)}
         >
-          <Typography variant="header" component="h3" gutterBottom>
+          <Typography variant="title" component="h3" gutterBottom>
               Datos
           </Typography>
           <div>
@@ -89,6 +124,8 @@ class QRForm extends React.PureComponent {
             <TextField
               id="cuit"
               label="CUIT"
+              required
+              error={cuitError}
               className={classes.textField}
               value={this.state.cuit}
               onChange={this.handleChange('cuit')}
@@ -98,6 +135,8 @@ class QRForm extends React.PureComponent {
             <TextField
               id="alias"
               label="AliasCBU"
+              required
+              error={aliasError}
               className={classes.textField}
               value={this.state.alias}
               onChange={this.handleChange('alias')}
