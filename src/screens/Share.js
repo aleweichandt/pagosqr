a1a76c83783e53b93942b056951f6b283fdb4cea
Alejandro Weichandt <aleweichandt@gmail.com>
2018-07-02 22:23:32 -0300

    added share view

--- src/screens/Share.js
+++ src/screens/Share.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Header from '../component/Header';
 import Footer from '../component/Footer';
+import QRCode from '../component/QRCode';
+import apiCall from '../api';
 
 class Share extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      qrData: undefined,
+    }
+  }
+  componentDidMount() {
+    const { match: { params: { id }}} = this.props;
+    const queryPath = `api/share/${id}`;
+    // const queryPath = `http://localhost:5000/api/share/${id}`;
+    apiCall(queryPath)
+      .then(({response}) =>  this.setState({qrData: response}))
+      .catch(err => console.log(err));
+  }
   render() {
+    const { qrData } = this.state;
+    const src = 'data:image/svg+xml,' + qrData;
     return (
       <React.Fragment>
-        <Header>Generá tu código de pagosQR</Header>
+        <Header>Tu código de pagosQR</Header>
+        { qrData ? 
+          <img src={src}/> : undefined }
+        {/* {qrData ? <QRCode payload={this.state.qrData}/> : undefined} */}
         <Footer/>
       </React.Fragment>
     );
