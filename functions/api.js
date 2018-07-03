bf1024fe404d71bf6edb0c19bdd9b3ca28b4462e
Alejandro Weichandt <aleweichandt@gmail.com>
2018-07-02 21:28:27 -0300

    added share api functions

--- functions/api.js
+++ functions/api.js
@@ -15,6 +15,29 @@ api.get('/test', (req, res) => {
   });
 });
 
+api.post('/share', (req, res) => {
+  const { body = {} } = req;
+  const { name , alias, cuit, city } = body;
+  const str = generator({name, alias ,cuit ,city });
+  //TODO encrypt
+  const hash = str;
+  res.send(hash);
+});
+
+api.get('/share/:hash', (req, res) => {
+  const { path: { hash } } = req;
+  //TODO decrypt
+  const str = hash;
+  QRCode.toString(str,
+  { type: 'svg', errorCorrectionLevel: 'L' }, (err, string) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(string);
+    }
+  });
+})
+
 api.post('/generate', (req, res) => {
   const { body = {} } = req;
   const { name , alias, cuit, city } = body;
